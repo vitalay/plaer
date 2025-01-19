b2ca699dd2bd3ebe8aba099b2a276d3c98776a62
vitalya <vitalay.efimov@gmail.com>
2025-01-19 15:52:38 +0500

    готово

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
-<script setup>
-import  Board  from './components/Board.vue'
-</script>
-
 <template>
-
- 
-<div class="app">
- <img class="logo" alt="Vue logo"src="./assets/logo.svg"  />
- <h1 class="h1">VUE 3</h1>
- <p>Запомни и открой все <br>зеленые квадратики</p>
- <Board /> 
-</div>
-
-
+  <div class="app">
+    <img class="logo" alt="Vue logo" src="./assets/logo.png">
+    
+    <h1 class="h1">Remember Me, Vue 3</h1>
+    <p>Запомни и открой все зеленые клетки</p>
+    
+    <Board />
+  </div>
 </template>
 
+<script>
+import Board from './components/Board.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Board,
+  }
+}
+</script>
+
 <style scoped>
-.app {
-  font-family: Helvetica, sans-serif;
-  text-align: center;
-  margin-top: 40px;
-}
-.logo {
-  width: 100px;
-  height: 100px;
-
-}
-
+  .app {
+    font-family: Helvetica, sans-serif;
+    text-align: center;
+    margin-top: 40px;
+  }
+  .logo {
+    width: 100px;
+  }
 </style>
--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,60 +1,93 @@
 <template>
   <div class="board-wrapper">
     <div class="board">
-      <borderItem v-for="i in 25" :key="'item-' + i" />
+      <BoardItem :game-status="gameStatus" v-for="field in fields" :field="field" :key="'item-' + field.id"
+        @selectField="selectField($event)"/>
     </div>
-
-    <p class="difficult"><strong>Сложность: {{ difficult }}</strong></p>
-    <button class="btn">Новая игра</button>
+    
+    <p class="difficult">Сложность: <strong>{{ difficult }}</strong></p>
+    <p class="win" v-if="isWin">Поздравляем! Продолжаем играть!</p>
+    <p class="fail" v-if="isFail">Вы проиграли. Попробуйте еще раз!</p>
+  
+    <button class="btn" @click="start" :disabled="!canStartGame">Старт</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import BorderItem from './BorderItem.vue'
+import BoardItem from './BoardItem.vue';
+import useGameInit from "@/components/composables/useGameInit";
+import useGameStart from "@/components/composables/useGameStart";
+import useGameProcess from "@/components/composables/useGameProcess";
+import { GAME_STATUS } from "@/constants";
+import { ref } from 'vue';
 
 export default {
   name: 'Board',
   props: {},
   components: {
-    BorderItem,
+    BoardItem,
   },
   setup() {
+    const number = 25;
+    const gameStatus = ref(GAME_STATUS.NONE);
+    
+    const { difficult, fields, init } = useGameInit(number);
+    
+    const { start, canStartGame } = useGameStart(init, fields, difficult, number, gameStatus);
 
-    const difficult = ref(5);
-
+    const { selectField, isWin, isFail } = useGameProcess(fields, gameStatus, difficult, start);
+    
     return {
-      difficult
+      number,
+      difficult,
+      fields,
+      init,
+      start,
+      gameStatus,
+      canStartGame,
+      selectField,
+      isWin,
+      isFail
     }
-  }
+  },
 }
 </script>
 
-
 <style scoped>
-.board-wrapper {
-  margin-bottom: 100px;
-}
-
-.board {
-  width: 300px;
-  background: #eee;
-  margin: 0, auto;
-}
-
-.difficult {
-  text-align: center;
-  margin-bottom: 20px;
-}
-
-.btn {
-  display: block;
-  margin: 0 auto;
-  padding: 10px 20px;
-  border: none;
-  background: #0eddcf;
-  color: #0914ec;
-  cursor: pointer;
-  font-size: 16px;
-}
+  .board-wrapper {
+    margin-bottom: 100px;
+  }
+  
+  .board {
+    width: 300px;
+    background: #eee;
+    margin: 0 auto;
+  }
+  
+  .btn {
+    background: #42b983cc;
+    color: white;
+    border: none;
+    border-radius: 2px;
+    padding: 10px 30px;
+    margin: 10px 0;
+    cursor: pointer;
+    outline: none;
+  }
+  
+  button:hover {
+    background: #42b983;
+  }
+  
+  button:disabled {
+    opacity: .5;
+  }
+  
+  .win {
+    color: #42b983;
+  }
+  
+  .fail {
+    color: #ff000055;
+  }
 </style>
